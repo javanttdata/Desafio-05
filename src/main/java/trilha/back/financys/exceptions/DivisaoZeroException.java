9252bb7888fb26f2d03de758d93517a910747f6a
javanttdata <javan.moisesgirardi.st@everis.nttdata.com>
2021-11-25 10:39:19 -0300

    tratamento de exceções

--- src/main/java/trilha/back/financys/exceptions/DivisaoZeroException.java
+++ src/main/java/trilha/back/financys/exceptions/DivisaoZeroException.java
@@ -1,6 +1,7 @@
 package trilha.back.financys.exceptions;
 
 public class DivisaoZeroException extends RuntimeException {
+    private static final long serialVersionUid = 1L;
 
     public DivisaoZeroException(String message){
 
